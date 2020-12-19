d1acdfbbcb8342c7f3842763b38ff553a391ce16
Nathan Cox <tsuujin@icloud.com>
2020-12-19 12:46:00 -0800

    Major refactor of emacs config

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -1,26 +1,5 @@
-;; (setq vc-follow-symlinks t)
-(require 'package)
-
-(setq package-enable-at-startup nil)
-
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives '("ublt" . "https://elpa.ubolonton.org/packages/"))
-
-(package-initialize)
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(require 'org-install)
-(require 'ob-tangle)
-
 (defun reload-config ()
 	"Reloads my literate config file."
 	(org-babel-load-file "~/.emacs.d/nathancox.org"))
 
-(setq max-lisp-eval-depth 2000)
-
 (reload-config)
-(put 'dired-find-alternate-file 'disabled nil)
