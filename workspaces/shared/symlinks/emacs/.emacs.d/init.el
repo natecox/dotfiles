755fab33aaa83ace99bab52029dd8c27804a0f08
Nathan Cox <ncox@covermymeds.com>
2021-12-15 20:05:47 -0800

    Update emacs config

--- workspaces/shared/symlinks/emacs/.emacs.d/init.el
+++ workspaces/shared/symlinks/emacs/.emacs.d/init.el
@@ -1,6 +1,7 @@
 (defun reload-config ()
 	"Reloads my literate config file."
-	(org-babel-load-file "~/.emacs.d/nathancox.org"))
+	(org-babel-load-file "~/.emacs.d/nathancox.org")
+    (if (file-readable-p "~/.emacs.d/local-config.org") (org-babel-load-file "~/.emacs.d/local-config.org")))
 
 (setq vc-follow-symlinks nil)
 (reload-config)
