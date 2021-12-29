5abdc18cac700548121b796ff3a3cc777742c383
Nathan Cox <tsuujin@gmail.com>
2021-12-29 11:16:40 -0800

    Remove straight, NANO
    Nano was a nice start with some good defaults, which proved to just be
    frustrating to maintain and modify.
    
    I've taken the defaults that I like from NANO and moved away from the
    rest of the package.
    
    Straight was also nice in that I could drop a block from my config and
    my next reload would behave like the package never existed, but by
    defaulting to pulling packages from their main branch it burned me
    multiple times; many packages do not treat `master` as production only
    and bugs made it through to me too many times.

--- workspaces/shared/symlinks/emacs/.emacs.d/init.el
+++ workspaces/shared/symlinks/emacs/.emacs.d/init.el
@@ -1,8 +1,20 @@
 (defun reload-config ()
-	"Reloads my literate config file."
-	(org-babel-load-file "~/.emacs.d/nathancox.org")
-    (if (file-readable-p "~/.emacs.d/local-config.org") (org-babel-load-file "~/.emacs.d/local-config.org")))
+  "Reloads my literate config file."
+  (org-babel-load-file "~/.emacs.d/nathancox.org")
+  (if (file-readable-p "~/.emacs.d/local-config.org") (org-babel-load-file "~/.emacs.d/local-config.org")))
 
 (setq vc-follow-symlinks nil)
 (reload-config)
 (put 'set-goal-column 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(exec-path-from-shell diminish use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
