e73b27b15a2724d985b33c3b4618dfe5e229ec0d
Nathan Cox <ncox@covermymeds.com>
2022-01-14 14:28:50 -0800

    General org updates

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -5,16 +5,3 @@
 
 (setq vc-follow-symlinks nil)
 (reload-config)
-(put 'set-goal-column 'disabled nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(exec-path-from-shell diminish use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
