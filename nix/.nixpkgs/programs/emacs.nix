679ce1228da60f37d51307003364d5ea20104875
Nate Cox <nate@natecox.dev>
2023-02-27 11:04:47 -0800

    Track emacs git

--- nix/.nixpkgs/programs/emacs.nix
+++ nix/.nixpkgs/programs/emacs.nix
@@ -5,11 +5,11 @@
       url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
     }))
   ];
-  
+
   programs.emacs = {
     enable = true;
-    
-    package = pkgs.emacsUnstable.override { nativeComp = true; };
+
+    package = pkgs.emacsGit;
     extraPackages = epkgs: with epkgs; [ vterm ];
   };
 }
