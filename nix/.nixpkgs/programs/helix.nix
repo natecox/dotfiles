e02a478084c05261bc60ba0a2f91a6b6ee9f2567
Nate Cox <nate@natecox.dev>
2023-04-08 11:45:50 -0700

    Update helix with a basic config

--- nix/.nixpkgs/programs/helix.nix
+++ nix/.nixpkgs/programs/helix.nix
@@ -4,5 +4,46 @@
     enable = true;
 
     package = pkgs.helix;
+
+    settings = 
+      {
+        theme = "kanagawa";
+        
+        editor = {
+          line-number = "relative";
+
+          soft-wrap = {
+            enable = true;
+          };
+
+          statusline = {
+            right = ["version-control" "diagnostics" "selections" "position" "position-percentage" "file-encoding"];
+          };
+
+          lsp = {
+            display-inlay-hints = true;
+          };
+
+          indent-guides = {
+            render = true;
+            character = "┊";
+            skip_levels = 1;
+          };
+        };
+
+        keys.insert = {
+          "C-g" = "normal_mode";
+        };
+      };
+
+    languages = [
+      {
+        name = "rust";
+        indent = { 
+          tab-width = 4;
+          unit = "\t";
+        };
+      }
+    ];
   };
 }
