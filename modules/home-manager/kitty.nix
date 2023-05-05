3745ff343ee3b82d69bc1bb57a339db0e92bf60d
Nate Cox <nate@natecox.dev>
2023-05-05 08:51:11 -0700

    Add some padding to kitty terminals

--- modules/home-manager/kitty.nix
+++ modules/home-manager/kitty.nix
@@ -10,12 +10,15 @@
     };
 
     settings = {
+      confirm_os_window_close = 0;
       enabled_layouts = "tall";
       tab_bar_edge = "bottom";
       tab_bar_style = "powerline";
       tab_powerline_style = "slanted";
       tab_title_template =
         "{title}{' :{}:'.format(num_windows) if num_windows > 1 else ''}";
+      window_padding_width = 10;
+      placement_strategy = "center";
     };
   };
 }
