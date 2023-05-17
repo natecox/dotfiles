4f665c625f2c41cc862b7d6a7478ee5d7c746fbd
Nate Cox <nate@natecox.dev>
2023-05-16 19:13:55 -0700

    fix: Reenable zsh config
    Keeping fish as the primary shell but reenabling zsh config so it's
    there if I need to drop into it to resolve a fish issue.

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -10,7 +10,7 @@
     ./kitty.nix
     ./neovim.nix
     ./starship.nix
-    # ./zsh.nix
+    ./zsh.nix
     ./fish.nix
   ];
 
@@ -27,29 +27,28 @@
 
   home.sessionVariables = {
     EDITOR = "hx";
-    NIX_TEMPLATES_ID = "$(security find-generic-password -w -s 'cli tokens' -a 'nix templates gist')";
+    NIX_TEMPLATES_ID =
+      "$(security find-generic-password -w -s 'cli tokens' -a 'nix templates gist')";
   };
 
   home.activation = lib.mkIf pkgs.stdenv.isDarwin {
-    copyApplications =
-      let
-        apps = pkgs.buildEnv {
-          name = "home-manager-applications";
-          paths = config.home.packages;
-          pathsToLink = "/Applications";
-        };
-      in
-      lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        baseDir="$HOME/Applications/Home Manager Apps"
-        if [ -d "$baseDir" ]; then
-          rm -rf "$baseDir"
-        fi
-        mkdir -p "$baseDir"
-        for appFile in ${apps}/Applications/*; do
-          target="$baseDir/$(basename "$appFile")"
-          $DRY_RUN_CMD cp ''${VERBOSE_ARG:+-v} -fHRL "$appFile" "$baseDir"
-          $DRY_RUN_CMD chmod ''${VERBOSE_ARG:+-v} -R +w "$target"
-        done
-      '';
+    copyApplications = let
+      apps = pkgs.buildEnv {
+        name = "home-manager-applications";
+        paths = config.home.packages;
+        pathsToLink = "/Applications";
+      };
+    in lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      baseDir="$HOME/Applications/Home Manager Apps"
+      if [ -d "$baseDir" ]; then
+        rm -rf "$baseDir"
+      fi
+      mkdir -p "$baseDir"
+      for appFile in ${apps}/Applications/*; do
+        target="$baseDir/$(basename "$appFile")"
+        $DRY_RUN_CMD cp ''${VERBOSE_ARG:+-v} -fHRL "$appFile" "$baseDir"
+        $DRY_RUN_CMD chmod ''${VERBOSE_ARG:+-v} -R +w "$target"
+      done
+    '';
   };
 }
